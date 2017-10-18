73ee63da4b673409ac80fa33aaaac7b87acfc61d
KLNSrikanth <KLNSrikanth@192.168.1.2>
2017-10-18 21:49:42 +0530

    Change SOP by KLNS 1

---
+++ Mvprj/src/test/java/Mavens/Mvprj/NewTest.java
@@ -0,0 +1,15 @@
+package Mavens.Mvprj;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class NewTest {
+	
+  @Test
+  public void ChromeTest() {
+	  System.setProperty("webdriver.chrome.driver", "/Users/KLNSrikanth/Downloads/chromedriver");
+	  AppTest.driver = new ChromeDriver();
+	  System.out.println("*********CHROMEE***********");
+	  AppTest.driver.quit();
+  }
+}
